b0bccaab31605e8dfd038319631af4a2c94baac2
sharjeelyunus <sharjeel924@gmail.com>
2022-01-16 17:24:47 +0500

    📦 NEW: Gernal

--- src/components/SocialLinks/SocialLinks.js
+++ src/components/SocialLinks/SocialLinks.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDiscord, faTwitter, faFacebook, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { SOCIALS } from '../../module/Gernal';
 import "./SocialLinks.css";
 
 const SocialLinks = () => {
     return (
         <div className='social'>
-            <a href="https://www.instagram.com/crack_the_solution/" rel="noreferrer" target="_blank">
+            <a href={SOCIALS.instagram} rel="noreferrer" target="_blank">
                 <FontAwesomeIcon icon={faInstagram} />
             </a>
-            <a href="https://www.linkedin.com/company/crackthesolution" target="_blank" rel="noreferrer">
+            <a href={SOCIALS.linkedin} target="_blank" rel="noreferrer">
                 <FontAwesomeIcon icon={faLinkedin} />
             </a>
-            <a href="https://www.facebook.com/crackthesolution/" target="_blank" rel="noreferrer">
+            <a href={SOCIALS.facebook} target="_blank" rel="noreferrer">
                 <FontAwesomeIcon icon={faFacebook} />
 
             </a>
-            <a href="https://twitter.com/cracksolution_" target="_blank" rel="noreferrer">
+            <a href={SOCIALS.twitter} target="_blank" rel="noreferrer">
                 <FontAwesomeIcon icon={faTwitter} />
             </a>
-            <a href="https://discord.gg/NmXeaxxu" target="_blank" rel="noreferrer">
+            <a href={SOCIALS.discord} target="_blank" rel="noreferrer">
                 <FontAwesomeIcon icon={faDiscord} />
             </a>
         </div>
---
+++ src/module/Gernal.js
@@ -0,0 +1,100 @@
+const SOCIALS = {
+    instagram: "https://www.instagram.com/crack_the_solution/",
+    discord: "https://discord.gg/NmXeaxxu",
+    linkedin: "https://www.linkedin.com/company/crackthesolution",
+    facebook: "https://www.facebook.com/crackthesolution/",
+    twitter: "https://twitter.com/cracksolution_",
+    devpost: "",
+    email: "mailto:crackthesolution@gmail.com",
+    mail: "crackthesolution@gmail.com"
+};
+
+const FORMS = {
+    VOLUNTEERING_FORM: "",
+    SPEAKER_FORM: "",
+    MENTOR_FORM: "",
+    PARTNER_FORM: ""
+};
+
+const Session = [
+    [
+        {
+            title: "",
+            speaker: "",
+            img: "",
+            role: "",
+            date: "",
+            time: "",
+            feature1: "",
+            feature2: "",
+            feature3: ""
+        }
+    ]
+];
+
+const SpeakersInfo = [
+    [
+        {
+            Name: "",
+            role: "",
+            github: "",
+            linkedin: "",
+            img: ""
+        }
+    ]
+];
+
+const JudgesInfo = [
+    [
+        {
+            Name: "",
+            role: "",
+            github: "",
+            linkedin: "",
+            img: ""
+        }
+    ]
+];
+
+const Prizeinfo = [
+    [
+        {
+            icon: "",
+            type: "",
+            content: ""
+        },
+    ],
+    [
+        //Array 2
+        {
+            icon: "",
+            type: "",
+            content: ""
+        },
+        {
+            icon: "",
+            type: "",
+            content: ""
+        },
+        {
+            icon: "",
+            type: "",
+            content: ""
+        }
+    ]
+
+];
+
+const sponsorLogos = [
+    [{ src: "" }, { src: "" }, { src: "" }],
+];
+
+export {
+    SOCIALS,
+    FORMS,
+    Session,
+    Prizeinfo,
+    sponsorLogos,
+    SpeakersInfo,
+    JudgesInfo
+};
