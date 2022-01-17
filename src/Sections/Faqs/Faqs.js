d2697fd714e231fdafa93d871a5eb4f3f1805255
sharjeelyunus <sharjeel924@gmail.com>
2022-01-17 20:48:58 +0500

    👌 IMPROVE: Faqs

--- src/Sections/Faqs/Faqs.js
+++ src/Sections/Faqs/Faqs.js
@@ -6,6 +6,10 @@ const data = {
     faqs: [
         { question: "What is Hackathon?", answer: "Hackathons are community events, where you get to interact with students from all around the world as well as forming teams to create future tech." },
 
+        { question: "Who can participate?", answer: "This event is targeted at university or college students, but any student is welcome to participate!" },
+
+        { question: "How does the application process work?", answer: "To apply to participate in solutionfest, you first need to go over to Apply form and register. Then go to devpost link to register your team and submissions. We also recommend that you join our discord server to stay on top of all the latest announcements and fun mini-events." },
+
         { question: "I hava no idea what coding is?", answer: "That's why you must attend our event, we will guide you through everything with awesome workshops, tech talks and many more." },
 
         { question: "What are the United Nations 17 Sustainable Development Goals?", answer: "Created by the United Nations in 2015 to be achieved by 2030, the 17 Sustainable Development Goals (SDGs) agreed upon by all 193 United Nations Member States aim to end poverty, ensure prosperity, and protect the planet. Check out: https://www.un.org/sustainabledevelopment/sustainable-development-goals/" },
@@ -14,13 +18,11 @@ const data = {
 
         { question: "I don't have a team to participate? ", answer: "You don't need one, prior to our event in discord you will meet many hackers exactly like you and join to form a team, if not you can participate alone." },
 
-        { question: "How to participate?", answer: "All you need is to fill our form above and join Discord, we will guide you through everything there." },
+        { question: "What size can a team be?", answer: "2 to 4 people - we suggest having a minimum of 3 people in a team. The Solutionfest team will organize matchmaking at the start of the hackathon, so no need to worry if you don't have a team before the event." },
 
-        { question: "I have more doubts?", answer: "Reach us directly at (email) we would happy to help you." },
+        { question: "I have more doubts?", answer: "Reach us directly at hello@solutionfest.tech we would happy to help you." },
 
-        { question: "Can I volunteer?", answer: "Yes we are looking for volunteers, fill the form (in footer section) we would be glad to have you." },
-
-        { question: "What are the conduct of guidilines?", answer: "We strongly follow HackClub code of conduct, Our team members will enforce this codde throughout the event. To see the code of conduct please visit the website footer." },
+        { question: "What are the conduct of guidelines?", answer: "We strongly follow HackClub code of conduct, Our team members will enforce this codde throughout the event. To see the code of conduct please visit the website footer." },
     ],
     backgroundColor: '#f4f5f9',
     titleColor: "#000",
