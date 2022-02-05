4c4c9ea3295dfa8fe254d2506b2ff9e4d70aa266
sharjeelyunus <sharjeel924@gmail.com>
2022-02-05 22:55:42 +0500

    👌 IMPROVE: Sponsors

--- src/Sections/Sponsors/Sponsors.js
+++ src/Sections/Sponsors/Sponsors.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import sponsorCover from '../../assets/sponsor-cover.png';
 import { SOCIALS } from '../../module/Gernal';
+import Google from '../../assets/google-developers.png';
 import './Sponsors.css';
 
 const Sponsors = () => {
@@ -21,7 +22,18 @@ const Sponsors = () => {
             </div>
             <div className='our_sponsors'>
                 <h2>Our sponsors</h2>
-                <p>Will be updated soon!</p>
+                <div className='sponsors_here'>
+                    <div className='diamond_sponsors'>
+                        <h3>DIAMOND SPONSORS</h3>
+                        <div className='sponsors_div'>
+                            <div>
+                                <a href="https://developers.google.com/" target="_blank" rel="noreferrer">
+                                    <img src={Google} alt="" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             {/* <div className='community_partners'>
                 <h2>Community Partners</h2>
